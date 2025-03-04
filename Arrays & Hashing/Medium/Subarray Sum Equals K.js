f809c6e5301b1bea0c98dbbc229cc414fa172744
taraskunyk <kunykty@gmail.com>
2025-03-04 11:21:14 -0600

    added 3Sum, Subarray Sum Equals K

---
+++ Arrays & Hashing/Medium/Subarray Sum Equals K.js
@@ -0,0 +1,16 @@
+const subarraySum = function(nums, k) {
+    let subNum = { 0: 1 };
+    let total = 0, count = 0;
+
+    for (const n of nums) {
+        total += n;
+
+        if (subNum[total - k] !== undefined) {
+            count += subNum[total - k];
+        }
+
+        subNum[total] = (subNum[total] || 0) + 1;
+    }
+
+    return count;
+};
---
+++ Two Pointers/Medium/3sum.js
@@ -0,0 +1,31 @@
+const threeSum = (nums) => {
+    nums.sort((a, b) => a - b);
+    const res = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) break;
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let left = i + 1;
+        let right = nums.length - 1;
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum > 0) {
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                res.push([nums[i], nums[left], nums[r]]);
+                left++;
+                right--;
+
+                while (left < right && nums[left] === nums[left - 1]) {
+                    left++;
+                }
+            }
+        }
+    }
+
+    return res;
+}
